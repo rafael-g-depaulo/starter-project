f0b73e9e50946aa51848a569f4964fd8e6c5b95f
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2022-08-31 12:36:41 -0300

    Add use-api hooks library
    Run `nx generate @nrwl/react:library useApi --style=styled-components --directory=hooks --no-component --importPath=@hooks/useApi --pascalCaseFiles`

---
+++ libs/hooks/use-api/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: "hooks-use-api",
+  preset: "../../../jest.preset.js",
+  transform: {
+    "^.+\\.[tj]sx?$": "babel-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../../coverage/libs/hooks/use-api",
+}
