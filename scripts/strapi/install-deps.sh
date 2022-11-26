7cea7c4b81804357cf9746e272ad7f32b80949a5
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2022-11-26 17:43:13 -0300

    T3

--- scripts/strapi/install-deps.sh
+++ scripts/strapi/install-deps.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
 # this shouldn't be necessary, but it currently is
-scripts/strapi/env-setup.sh
+####### CONTENT OF ENV-SETUP.SH ################
+####### CONTENT OF ENV-SETUP.SH ################
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+
+# load nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# install node
+nvm install
+nvm use
+# nvm install 16.15.1
+
+# install yarn
+npm i -g yarn
+
+# setup permissions for whole all files in project folder. this sometimes is necessary
+sudo chmod -R 777 .
+####### CONTENT OF ENV-SETUP.SH ################
+####### CONTENT OF ENV-SETUP.SH ################
+
 
 nvm --version
 node --version
